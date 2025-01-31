abeb5ede3f9ea861e9fb5db7285b3af5c8eaeb45
gaetcot <47340433+gaetcot@users.noreply.github.com>
2025-01-31 09:31:52 +0000

    Inizializza il progetto con l'esempio Hello World di Anchor

--- surveytrend_token/migrations/deploy.js
+++
@@ -1,7 +0,0 @@
-const anchor = require('@project-serum/anchor');
-
-module.exports = async function(provider) {
-  anchor.setProvider(provider);
-  const program = anchor.workspace.SurveytrendToken;
-  console.log("Deploying token program:", program.programId.toString());
-};
